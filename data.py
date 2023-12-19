49528ef91c123e98a0cbbcf060633e59bf4a79f1
savacharya <savacharya22@gmail.com>
2023-12-19 17:15:45 +1100

    separate data file

---
+++ data.py
@@ -0,0 +1,109 @@
+def movie_quiz():
+    questions = {
+    1: {
+        'que': 'What is the capital of Japan?',
+        'a': 'Beijing',
+        'b': 'Seoul',
+        'c': 'Tokyo',
+        'd': 'Bangkok',
+        'correct': 'c'
+    },
+    2: {
+        'que': 'Which element has the chemical symbol "O"?',
+        'a': 'Oxygen',
+        'b': 'Gold',
+        'c': 'Silver',
+        'd': 'Carbon',
+        'correct': 'a'
+    },
+    3: {
+        'que': 'Who wrote "Romeo and Juliet"?',
+        'a': 'Charles Dickens',
+        'b': 'Jane Austen',
+        'c': 'William Shakespeare',
+        'd': 'Mark Twain',
+        'correct': 'c'
+    },
+    4: {
+        'que': 'What is the largest mammal on Earth?',
+        'a': 'Elephant',
+        'b': 'Blue Whale',
+        'c': 'Giraffe',
+        'd': 'Hippopotamus',
+        'correct': 'b'
+    },
+    5: {
+        'que': 'Which planet is known as the "Morning Star" or "Evening Star"?',
+        'a': 'Mars',
+        'b': 'Venus',
+        'c': 'Jupiter',
+        'd': 'Saturn',
+        'correct': 'b'
+    },
+    6: {
+        'que': 'In what year did the Titanic sink?',
+        'a': '1912',
+        'b': '1923',
+        'c': '1905',
+        'd': '1931',
+        'correct': 'a'
+    },
+    # Add more questions as needed
+    
+    
+}
+    return questions
+
+
+def science_quiz():
+    questions = {
+    1: {
+        "que": "What is the smallest planet in our solar system?",
+        "a": "Mercury",
+        "b": "Venus",
+        "c": "Earth",
+        "d": "Mars",
+        "correct": "a"
+    },
+    2: {
+        "que": "What is the process by which plants make their own food?",
+        "a": "Photosynthesis",
+        "b": "Respiration",
+        "c": "Transpiration",
+        "d": "Germination",
+        "correct": "a"
+    },
+    3: {
+        "que": "What is the main component of air?",
+        "a": "Nitrogen",
+        "b": "Oxygen",
+        "c": "Carbon dioxide",
+        "d": "Water vapor",
+        "correct": "b"
+    },
+    4: {
+        "que": "What is the largest bone in the human body?",
+        "a": "Femur",
+        "b": "Tibia",
+        "c": "Humerus",
+        "d": "Skull",
+        "correct": "a"
+    },
+    5: {
+        "que": "What is the study of rocks and minerals called?",
+        "a": "Paleontology",
+        "b": "Geology",
+        "c": "Meteorology",
+        "d": "Oceanography",
+        "correct": "b"
+    },
+    6: {
+        "que": "What is the process by which water changes from a liquid to a gas?",
+        "a": "Condensation",
+        "b": "Evaporation",
+        "c": "Precipitation",
+        "d": "Sublimation",
+        "correct": "b"
+    }
+}
+    return questions
